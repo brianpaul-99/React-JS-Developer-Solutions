c9f28a1d6a5ca2a7e633587594cf3c19142b5112
brianpaul-99 <60050809+brianpaul-99@users.noreply.github.com>
2022-02-20 12:51:09 -0500

    Create question3.ts

---
+++ Question3/question3.ts
@@ -0,0 +1,12 @@
+if (recipe == "SPANISH") {
+  fudge = SPANISH_FUDGE;
+} else if (recipe == "FRENCH") {
+  fudge = FRENCH_FUDGE;
+  chocolate = 7;
+} else if (recipe == "ENGLISH") {
+  fudge = ENGLISH_FUDGE;
+} else {
+  fudge = 1;
+}
+amt = base * fudge;
+sugar = 2 * bottom(amt) + top(amt) * 1.17;
