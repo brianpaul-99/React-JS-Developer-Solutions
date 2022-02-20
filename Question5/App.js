096be8e039496b9512552a903bbf1a5d81072cc2
brianpaul-99 <60050809+brianpaul-99@users.noreply.github.com>
2022-02-20 12:53:33 -0500

    Create App.js

---
+++ Question5/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+function App() {
+  const [word, setWord] = useState("");
+  const [rhymeList, setRhymeList] = useState([]);
+
+  function getRhymes() {
+    fetch("https://api.datamuse.com/words?rel_rhy=" + word.toLowerCase())
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setRhymeList(res);
+      });
+  }
+
+  return (
+    <div className="App">
+      <div className="inputContainer">
+        <div className="prompt">Type a word to rhyme!</div>
+        <input
+          onChange={(event) => {
+            event.preventDefault();
+            setWord(event.target.value);
+          }}
+          className="input"
+        />
+        <button onClick={getRhymes} className="button" type="button">
+          Rhyme word
+        </button>
+      </div>
+      <div className="listContainer">
+        <div className="successContainer">
+          <div className="successText">
+            {rhymeList.length == 0
+              ? "Type a valid word"
+              : `Found ${rhymeList.length} matching words!!!`}
+          </div>
+        </div>
+        {rhymeList.map((rhyme, i) => (
+          <div className="row">
+            <div className="rowText">{rhyme.word}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
